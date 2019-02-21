aa1e6d8f9c92f37a0960b1419d7bf5fab1917279
diptasahacse <diptasaha.lpu.cse@gmail.com>
2019-02-22 02:06:12 +0530

    TextView Access From Java in Android

--- app/src/main/java/com/example/user/textviewdemo/MainActivity.java
+++ app/src/main/java/com/example/user/textviewdemo/MainActivity.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView1;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textViewid1);
+        textView2 = findViewById(R.id.textViewid2);
+
+
+        textView1.setText("Dipta saha");
+        textView2.setText("Jugol Kumar");
 
 
     }
